e1fe67218541333d3fc42ab8df45696ac66dd7db
noah <nomaxx117@gmail.com>
2019-12-29 17:23:21 -0600

    Added quality logging to server_test.rs

--- examples/server_test.rs
+++ examples/server_test.rs
@@ -3,7 +3,8 @@ use async_std::sync::{channel, Sender};
 use jetson_network::launch;
 use jetson_network::prelude::{SendingMessage, SendingMessagePayload};
 use std::time::Duration;
-
+use log::LevelFilter;
+use std::io::Write;
 // {"payload":{"Drive":{"left":5,"right":5}}}
 
 async fn ping(sender: Sender<SendingMessage>) {
@@ -14,6 +15,16 @@ async fn ping(sender: Sender<SendingMessage>) {
 }
 
 fn main() {
+    pretty_env_logger::formatted_builder()
+        .filter_level(LevelFilter::Debug)
+        .format(|buf, record| {
+            writeln!(buf, "[{}]\tModule: {}\tLine: {}\t{}",
+                     record.level(),
+                     record.module_path().expect("Couldn't find module path"),
+                     record.line().expect("Couldn't find line"),
+                     record.args())
+        }).init();
+
     let (sr, rr) = channel(100);
     let (ss, rs) = channel(100);
 
